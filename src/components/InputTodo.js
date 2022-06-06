4250f9a845eab10dbe0db07999ffa6c6cbb6bd39
yayner <yayner2002@gmail.com>
2022-06-06 22:14:08 +0300

    Handling react form for more than one input tags

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,22 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
 class InputTodo extends Component {
-    state = {
-        title: ""
-      };
-      onChange = e => {
-        this.setState({
-          title: e.target.value
-        });
-      };
-    render() { 
-        return (
-            <form>
-                <input type="text" placeholder="Add Todo..." value={this.state.title} onChange={this.onChange} />
-                <button>Submit</button>
-            </form>
-        );
-    }
+  state = {
+    title: "",
+  }
+  onChange = e => {
+    this.setState({
+        [e.target.name]: e.target.value,
+      })
+  }
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Add todo..."
+          value={this.state.title}
+          name="title"
+          onChange={this.onChange}
+        />
+        <button>Submit</button>
+      </form>
+    )
+  }
 }
- 
-export default InputTodo;
+export default InputTodo
