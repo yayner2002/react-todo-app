88064586ce4f0d0ae317f1e4d2a3a9d45df52d86
yayner <yayner2002@gmail.com>
2022-06-06 21:56:53 +0300

    Add event to the input form and grap its value of the target element

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -4,10 +4,15 @@ class InputTodo extends Component {
     state = {
         title: ""
       };
+      onChange = e => {
+        this.setState({
+          title: e.target.value
+        });
+      };
     render() { 
         return (
             <form>
-                <input type="text" placeholder="Add Todo..." value={this.state.title} />
+                <input type="text" placeholder="Add Todo..." value={this.state.title} onChange={this.onChange} />
                 <button>Submit</button>
             </form>
         );
