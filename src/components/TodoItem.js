a877b30ca99a22a66a8937015aa388dc1c8a2477
yayner <yayner2002@gmail.com>
2022-06-07 17:20:31 +0300

    Finalize css by using object destructuring

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,23 +6,24 @@ class TodoItem extends Component {
     render() {
         const completedStyle = {
             fontStyle: "italic",
-            color: "red",
+            color: "595959",
             opacity: 0.4,
             textDecoration: "line-through",
           } 
+          const { completed, id, title } = this.props.todo
           return (
             <li className={styles.item}>
               <input
                 type="checkbox"
                 className={styles.checkbox}
-                checked={this.props.todo.completed}
-                onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+                checked={completed}
+                onChange={() => this.props.handleChangeProps(id)}
               />
-              <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+              <button onClick={() => this.props.deleteTodoProps(id)}>
                 Delete
               </button>
-              <span style={this.props.todo.completed ? completedStyle : null}>
-                {this.props.todo.title}
+              <span style={completed ? completedStyle : null}>
+                {title}
               </span>
             </li>
           )
