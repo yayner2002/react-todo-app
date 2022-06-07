0b2c84f067630382ece474110256f4510b5e036c
yayner <yayner2002@gmail.com>
2022-06-07 15:51:21 +0300

    Add classNama to the form

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -22,16 +22,17 @@ class InputTodo extends Component {
   }
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          placeholder="Add todo..."
-          value={this.state.title}
-          name="title"
-          onChange={this.onChange}
-        />
-        <button>Submit</button>
-      </form>
+      <form onSubmit={this.handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={this.state.title}
+        name="title"
+        onChange={this.onChange}
+      />
+      <button className="input-submit">Submit</button>
+    </form>
     )
   }
 }
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -59,15 +59,21 @@ class TodoContainer extends Component {
           todos: [...this.state.todos, newTodo]
         });
       };
-    render() { 
+      render() {
         return (
-            <div>
-                <Header />
-                <InputTodo addTodoProps={this.addTodoItem} />
-                <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} deleteTodoProps={this.delTodo}/>
+          <div className="container">
+            <div className="inner">
+              <Header />
+              <InputTodo addTodoProps={this.addTodoItem} />
+              <TodosList
+                todos={this.state.todos}
+                handleChangeProps={this.handleChange}
+                deleteTodoProps={this.delTodo}
+              />
             </div>
+          </div>
         );
-    }
+      }
 }
  
 export default TodoContainer;
