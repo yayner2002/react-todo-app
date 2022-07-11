849135cb687e43eb93fccef783f1b1254bf22229
yayner <yayner2002@gmail.com>
2022-07-11 23:06:30 +0300

    Adding SVG Icons

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,28 +1,29 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import { FaPlusCircle } from "react-icons/fa";
 
-const InputTodo = props => {
+const InputTodo = (props) => {
   const [inputText, setInputText] = useState({
     title: "",
-  })
+  });
 
-  const onChange = e => {
+  const onChange = (e) => {
     setInputText({
       ...inputText,
       [e.target.name]: e.target.value,
-    })
-  }
+    });
+  };
 
-  const handleSubmit = e => {
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (inputText.title.trim()) {
-      props.addTodoProps(inputText.title)
+      props.addTodoProps(inputText.title);
       setInputText({
         title: "",
-      })
+      });
     } else {
-      alert("Please write item")
+      alert("Please write item");
     }
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit} className="form-container">
@@ -34,9 +35,9 @@ const InputTodo = props => {
         name="title"
         onChange={onChange}
       />
-      <button className="input-submit">Submit</button>
+      <button className="input-submit"><FaPlusCircle /></button>
     </form>
-  )
-}
+  );
+};
 
-export default InputTodo
+export default InputTodo;
--- src/functionBased/components/TodoItem.js
+++ src/functionBased/components/TodoItem.js
@@ -1,41 +1,42 @@
-import React, { useState, useEffect } from "react"
-import styles from "./TodoItem.module.css"
+import React, { useState, useEffect } from "react";
+import styles from "./TodoItem.module.css";
+import { FaTrash } from "react-icons/fa";
 
-const TodoItem = props => {
-  const [editing, setEditing] = useState(false)
+const TodoItem = (props) => {
+  const [editing, setEditing] = useState(false);
 
   const handleEditing = () => {
-    setEditing(true)
-  }
+    setEditing(true);
+  };
 
-  const handleUpdatedDone = event => {
+  const handleUpdatedDone = (event) => {
     if (event.key === "Enter") {
-      setEditing(false)
+      setEditing(false);
     }
-  }
+  };
 
   const completedStyle = {
     fontStyle: "italic",
     color: "#595959",
     opacity: 0.4,
     textDecoration: "line-through",
-  }
+  };
 
-  const { completed, id, title } = props.todo
+  const { completed, id, title } = props.todo;
 
-  let viewMode = {}
-  let editMode = {}
+  let viewMode = {};
+  let editMode = {};
 
   if (editing) {
-    viewMode.display = "none"
+    viewMode.display = "none";
   } else {
-    editMode.display = "none"
+    editMode.display = "none";
   }
   useEffect(() => {
     return () => {
-      console.log("Cleaning up...")
-    }
-  }, [])
+      console.log("Cleaning up...");
+    };
+  }, []);
   return (
     <li className={styles.item}>
       <div onDoubleClick={handleEditing} style={viewMode}>
@@ -45,7 +46,7 @@ const TodoItem = props => {
           checked={completed}
           onChange={() => props.handleChangeProps(id)}
         />
-        <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
+        <button onClick={() => props.deleteTodoProps(id)}><FaTrash /></button>
         <span style={completed ? completedStyle : null}>{title}</span>
       </div>
       <input
@@ -53,13 +54,13 @@ const TodoItem = props => {
         style={editMode}
         className={styles.textInput}
         value={title}
-        onChange={e => {
-          props.setUpdate(e.target.value, id)
+        onChange={(e) => {
+          props.setUpdate(e.target.value, id);
         }}
         onKeyDown={handleUpdatedDone}
       />
     </li>
-  )
-}
+  );
+};
 
-export default TodoItem
+export default TodoItem;
