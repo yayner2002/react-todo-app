184394d7f37daceb496885970ba178e45535311c
yayner <yayner2002@gmail.com>
2022-07-13 14:37:40 +0300

    React routing

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -3,7 +3,9 @@ import Header from "./Header";
 import InputTodo from "./InputTodo";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
-
+import { Route, Routes } from "react-router-dom";
+import About from "../pages/About";
+import NotMatch from "../pages/NoMatch";
 const TodoContainer = () => {
   const [todos, setTodos] = useState(getInitialTodos());
 
@@ -56,22 +58,33 @@ const TodoContainer = () => {
 
   function getInitialTodos() {
     // getting stored items
-    const temp = localStorage.getItem("todos")
-    const savedTodos = JSON.parse(temp)
-    return savedTodos || []
+    const temp = localStorage.getItem("todos");
+    const savedTodos = JSON.parse(temp);
+    return savedTodos || [];
   }
 
   return (
     <div className="container">
       <div className="inner">
-        <Header />
-        <InputTodo addTodoProps={addTodoItem} />
-        <TodoList
-          todos={todos}
-          handleChangeProps={handleChange}
-          deleteTodoProps={delTodo}
-          setUpdate={setUpdate}
-        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <InputTodo addTodoProps={addTodoItem} />
+                <TodoList
+                  todos={todos}
+                  handleChangeProps={handleChange}
+                  deleteTodoProps={delTodo}
+                  setUpdate={setUpdate}
+                />
+              </>
+            }
+          />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotMatch />} />
+        </Routes>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
-import './functionBased/App.css';
+import "./functionBased/App.css";
 import { createRoot } from "react-dom/client";
 import TodoContainer from "./functionBased/components/TodoContainer";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router>
+      <TodoContainer />
+    </Router>
   </React.StrictMode>
 );
