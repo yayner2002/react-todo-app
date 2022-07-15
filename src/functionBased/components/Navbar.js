714c1dd34c91985ea2b555de4c96674b649785cb
yayner <yayner2002@gmail.com>
2022-07-15 16:19:31 +0300

    Add Humbergermenu open to the navBar

--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../App.css";
 
 const Navbar = () => {
+  const [navbarOpen, setnavbarOpen] = useState(false);
   const links = [
     {
       id: 1,
@@ -15,9 +16,14 @@ const Navbar = () => {
       text: "About",
     },
   ];
+
+  const handleTogggle = () => {
+    setnavbarOpen((prevState) => !prevState);
+  };
   return (
     <nav className="navBar">
-      <ul>
+      <button onClick={handleTogggle}>{navbarOpen ? "close" : "open"}</button>
+      <ul className={`menuNav ${navbarOpen ? "showMenu" : ""}`}>
         {links.map((link) => {
           return (
             <li key={link.id}>
