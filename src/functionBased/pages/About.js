8cd300a254527cde6b3ed35e84c7518cd79fe3a1
yayner <yayner2002@gmail.com>
2022-07-15 15:58:37 +0300

    Routing with react router v6

--- src/functionBased/pages/About.js
+++ src/functionBased/pages/About.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { matchRoutes } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
+import SinglePage from "./SinglePage";
 
-const About = (props) => {
-  console.log(matchRoutes());
-  return <div>hello from about page</div>;
+const About = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="about-app">About App</Link>
+        </li>
+        <li>
+          <Link to="about-author">About Author</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path=":slug" element={<SinglePage  />}/>
+      </Routes>
+    </div>
+  );
 };
 export default About;
